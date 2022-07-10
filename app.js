338742e22c64dc6d8d0d8eccfa0ef31777f78192
abbos0959 <abbosakromovich777@gmail.com>
2022-07-10 10:17:58 -0700

    newsRoter

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const express = require("express");
+const express=require("express")
 const app = express();
 
+
+
+app.use(express.json())
 app.use(express.static("public"))
 app.use("/css",express.static(__dirname + `public/css`))
+app.use("/img",express.static(__dirname + `public/img`))
+app.use("/js",express.static(__dirname + `public/js`))
+
+
+
+app.set("views","./src/views")
+app.set("view engine","ejs")
+const newsRouter=require("./src/routes/news")
+
+app.use("/",newsRouter)
 
 
 app.listen(6000, () => {
    console.log("server ishladi");
 });
+
